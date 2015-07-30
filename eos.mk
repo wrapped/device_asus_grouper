bae309ea147b1efb75d376cdcb6f6a3e025082d6
Daniel Johansson <ddroidx@gmail.com>
2015-07-30 18:11:09 -0500

    Bring grouper into the now! Add dependencies and make eos.mk pretty.

--- eos.mk
+++ eos.mk
@@ -1,13 +1,29 @@
-$(call inherit-product, device/asus/grouper/full_grouper.mk)
+# Release name
+PRODUCT_RELEASE_NAME := Nexus7
 
-# Enhanced NFC
-$(call inherit-product, vendor/eos/config/nfc_enhanced.mk)
-
-# Inherit some common Eos stuff.
-$(call inherit-product, vendor/eos/config/common_full_tablet_wifionly.mk)
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
 
 # Copy Bootanimation
 PRODUCT_COPY_FILES += \
 vendor/eos/prebuilt/common/bootanimation/1080.zip:system/media/bootanimation.zip
 
+# Inherit some common EOS stuff.
+$(call inherit-product, vendor/eos/config/common_full_tablet_wifionly.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/eos/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/grouper/full_grouper.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := grouper
 PRODUCT_NAME := eos_grouper
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:5.1/LMY47D/1743759:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.1 LMY47D 1743759 release-keys"
